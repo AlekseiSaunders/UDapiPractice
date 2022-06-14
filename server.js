5fdd2d4ed19bdc82ef7b27a73f856bc5e23d138d
Aleksei Saunders <alekseisaunders@gmail.com>
2022-06-14 16:44:40 -0600

    add packages and links to json file. add get for api route

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
+const { readFileSync } = require('fs');
+const madFish = JSON.parse(readFileSync('./fish.json'));
+const cors = require('cors');
 const ejs = require('ejs');
 const PORT = 8000;
 
 app.set('view engine', 'ejs');
+app.use(cors());
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-  response.send(`<h1>Hello</h1>`);
+  response.sendFile(__dirname + '/index.html');
 });
 
-app.listen(process.env.PORT || PORT, () =>
-  console.log(`Server running, listening on Port: ${PORT}`)
-);
+app.get('/api/:fishGenera', (request, response) => {
+  const fishGen = request.params.fishGenera.toLowerCase();
+  if (madFish[fishGen]) {
+    console.log(madFish[fishGen]);
+    response.status(200);
+    response.json(madFish[fishGen]);
+  } else {
+    response.json(madFish['placeHolder']);
+    // res.status(400).json({message: 'Fish Genera Not Found'})
+  }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
